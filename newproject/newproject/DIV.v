e149483f14ee2d01389ebf115cf7d3dca67e98ec
chloeath <chloe.g.atherton@gmail.com>
2024-03-14 09:21:07 -0400

    'changes'

--- newproject/newproject/DIV.v
+++ newproject/newproject/DIV.v
@@ -1,44 +1,32 @@
 module DIV(
+input wire [31:0] dividend, divisor,
 
-input [31:0] A, B, 
 output reg [63:0] Divresult
-
+      
 );
+      
+integer i;
+reg [31:0] m, a, q, temp;
+      always @(dividend or divisor)
+            begin
+            a= 32'd0;
+            q= dividend;
+            m= divisor;
+            for(i=0;i<32;i=i+1)
+                  begin
+                  a={a[30:0],q[31]};
+                  temp = a-m;
+                  if (temp[31]==1)begin
+                        q={q[30:0],1'b0};
+                  end
+                  else begin
+                        q={q[30:0],1'b1};
+                        a=temp;
+                  end
+                  end
+                        
+                  Divresult = {a, q};
 
-    reg [31:0] Q, Rem, Div;
-    reg [31:0] temp_Rem;		//define file specific varibles
-    integer y;
-
-    always @(*) begin
-        Q = 0;
-        Rem = 0;
-        Div = B;
-        temp_Rem = A; //initilize all veriables starting with A
-
-
-        for (y = 31; y >= 0; y = y - 1) begin
-				//loop through all bits from input register
-				
-            //start the non-restorative formula shifting remainder left by one bit 
-            Rem = Rem << 1;
-				
-            Rem[0] = temp_Rem[31]; //prepaire the next bit to be to used from divisor
-				
-            temp_Rem = temp_Rem << 1; //set temp remainder after shift, non restorative so no need to reset
-				
-            
-				//loop condition to determine if remainder will be positive or negative to determine if 0 or 1 will be added to Q
-            if (Rem >= Div) begin
-					 //calculate new remainder after subtracting the div
-                Rem = Rem - Div;
-                Q = (Q << 1) | 1; // Set the last bit to 1 
-            end else begin
-                Q = Q << 1; // Set the last bit to 0
-            end
-        end
-
-        // Adding together quotent with remainder for final 
-       Divresult[31:0] = Q;
-       Divresult[63:32] = Rem; 
-    end
+                        
+                  end
 endmodule
--- newproject/newproject/MUL.v
+++ newproject/newproject/MUL.v
@@ -1,55 +1,31 @@
-
-
-module MUL(
-	input signed [31:0] A, B,
-	output [63:0] mulresult
-);
-	reg[2:0] cc[(32/2)-1: 0]; //carry collection
-	reg[31:0] pp[(32/2)-1: 0]; //partial product
-	reg [63:0] spp[(32/2)-1: 0]; //signed partial product
-	reg [63: 0] product; //final
-	
-	wire[32:0] inv_a;
-	assign inv_a = {~A[31], ~A} + 1; //2's compiment of A to be used for booth's algo
-	
-	integer j,i;
-	
-	always @ (A or B or inv_a) begin
-		cc[0] = {B[1], B[0], 1'b0}; //concat two first bits, last bit is carry
-		
-			for (j=1;j<(32/2);j=j+1)
-			begin
-				cc[j] = {B[2*j+1], B[2*j], B[2*j-1]};
-			end
-			
-			//Calculate the partial product for each of the bits depending on the bit-pair encoding found above
-			for (j=0; j<(32/2); j=j+1) 
-			begin
-				case(cc[j])
-				
-				//+1M
-				3'b001, 3'b010 : pp[j] = {A[32-1], A};
-				//+2M
-				3'b011 : pp[j] = {A, 1'b0};
-				//-2M
-				3'b100 : pp[j] = {inv_a[32-1:0], 1'b0};
-				//-1M
-				3'b101, 3'b110 : pp[j] = {inv_a[32-1], inv_a};
-				//0
-				default : pp[j] = 0;
-				endcase
-				//Convert them to signed partial products
-				spp[j] = $signed(pp[j]) << (j*2);
-			end
-			product = spp[0];
-			
-			//sum up the products to obtain the final result
-			for (j=1; j<(32/2); j = j+1)
-			begin
-				product = product +spp[j];
-			end
-		end
-		assign mulresult = product;
-endmodule
-
-			
+module MUL(X, Y, Zin);
+ 
+    input signed [31:0] X, Y;
+    output reg [63:0] Zin;
+   
+    reg signed [63:0] Z; // Temporary register for calculations
+    reg [1:0] temp;
+    integer i;
+    reg E1; // Extra bit for Booth's algorithm
+   
+    always @(*) begin
+        Z = 64'd0;
+        Z[31:0] = X;
+        E1 = 0;
+       
+        for (i = 0; i < 32; i = i + 1) begin
+            temp = {Z[0], E1}; // Consider LSB of Z and extra bit
+           
+            case (temp)
+                2'b01: Z[63:32] = Z[63:32] + Y; // Add Y for 01
+                2'b10: Z[63:32] = Z[63:32] - Y; // Subtract Y for 10
+                default: ;
+            endcase
+           
+            E1 = Z[0]; // Update E1 with the LSB of Z before shift
+            Z = Z >>> 1; // Arithmetic right shift
+        end
+       
+        Zin = Z;
+    end
+endmodule	
